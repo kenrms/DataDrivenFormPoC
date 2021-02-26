bdf1dc4de3b4a37b408bc5b3b2c13da5136c6928
Ken Ramos <kenneth.ramos@avanade.com>
2021-02-25 20:07:51 -0800

    Code Rub

--- DataDrivenFormPoC/Startup.cs
+++ DataDrivenFormPoC/Startup.cs
@@ -17,8 +17,6 @@ namespace DataDrivenFormPoC
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
@@ -33,7 +31,6 @@ namespace DataDrivenFormPoC
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -43,7 +40,6 @@ namespace DataDrivenFormPoC
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
