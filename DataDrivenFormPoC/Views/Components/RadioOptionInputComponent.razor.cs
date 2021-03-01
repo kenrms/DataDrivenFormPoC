1c210d59a7d3753cfff3b3edc28d9fddaff38db5
Ken Ramos <v-keramos@microsoft.com>
2021-03-01 12:50:53 -0800

    Code Rub - RadioQuestionComponent

---
+++ DataDrivenFormPoC/Views/Components/RadioOptionInputComponent.razor.cs
@@ -0,0 +1,23 @@
+﻿using DataDrivenFormPoC.Models;
+using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
+
+namespace DataDrivenFormPoC.Views.Components
+{
+    public partial class RadioOptionInputComponent : ComponentBase
+    {
+        [Parameter]
+        public Option Option { get; set; }
+
+        [Parameter]
+        public string Name { get; set; }
+
+        [Parameter]
+        public EventCallback<ComponentBase> Callback { get; set; }
+
+        protected async override Task OnInitializedAsync()
+        {
+            await this.Callback.InvokeAsync(this);
+        }
+    }
+}
---
+++ DataDrivenFormPoC/Views/Components/RadioQuestionComponent.razor.cs
@@ -0,0 +1,44 @@
+﻿using DataDrivenFormPoC.Models;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataDrivenFormPoC.Views.Components
+{
+    public partial class RadioQuestionComponent : ComponentBase, IOptionResponder
+    {
+        [Parameter]
+        public Question Question { get; set; }
+
+        [Parameter]
+        public EventCallback<IOptionResponder> Callback { get; set; }
+
+        protected async override Task OnInitializedAsync()
+        {
+            await this.Callback.InvokeAsync(this);
+        }
+
+        public IList<OptionResponse> GetOptionResponses()
+        {
+            var optionResponses = new List<OptionResponse>();
+
+            foreach (var option in this.Question.Options)
+            {
+                var optionResponse = new OptionResponse
+                {
+                    Question = Question,
+                    Option = Question.Options.First(),
+                };
+
+                // TODO set response
+
+                optionResponses.Add(optionResponse);
+            }
+
+            return optionResponses;
+        }
+
+
+    }
+}
