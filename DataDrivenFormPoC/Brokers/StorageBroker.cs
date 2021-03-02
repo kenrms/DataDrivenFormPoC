30323d9fa84b96831b1131692aa36ab0c22ecd23
Ken Ramos <v-keramos@microsoft.com>
2021-03-02 09:07:07 -0800

    Code rub

--- DataDrivenFormPoC/Brokers/StorageBroker.cs
+++ DataDrivenFormPoC/Brokers/StorageBroker.cs
@@ -30,66 +30,85 @@ namespace DataDrivenFormPoC.Brokers
 
             if (!this.Forms.Any())
             {
-                var testForm = new Form
-                {
-                    Id = Guid.NewGuid(),
-                    Questions = {
-                        new Question
-                        {
-                            Id = Guid.NewGuid(),
-                            IsRequired = false,
-                            QuestionText = "Question 1",
-                            ResponseType = ResponseType.RawText,
-                            Options = {
-                                new Option()
-                            },
-                            Order = 1,
-                        },
-                        new Question
-                        {
-                            Id = Guid.NewGuid(),
-                            IsRequired = false,
-                            QuestionText = "Question 2",
-                            ResponseType = ResponseType.SingleChoiceDropDown,
-                            Options = {
-                                new Option{ Value = "A"},
-                                new Option{ Value = "B"},
-                                new Option{ Value = "C"},
-                            },
-                            Order = 2,
-                        },
-                        new Question
-                        {
-                            Id = Guid.NewGuid(),
-                            IsRequired = false,
-                            QuestionText = "Question 3",
-                            ResponseType = ResponseType.SingleChoiceRadio,
-                            Options = {
-                                new Option{ Value = "Yes"},
-                                new Option{ Value = "No"},
-                            },
-                            Order = 3,
-                        },
-                        new Question
-                        {
-                            Id = Guid.NewGuid(),
-                            IsRequired = false,
-                            QuestionText = "Question 4",
-                            ResponseType = ResponseType.MultipleChoice,
-                            Options = {
-                                new Option{ Value = "Pepperoni"},
-                                new Option{ Value = "Sausage"},
-                                new Option{ Value = "Chicken"},
-                                new Option{ Value = "Mushrooms"},
-                            },
-                            Order = 4,
-                        },
-                    }
-                };
-
-                this.Forms.Add(testForm);
-                this.SaveChanges();
+                CreateSeedFormData();
             }
+        }
+
+        private void CreateSeedFormData()
+        {
+            var form = new Form
+            {
+                Id = Guid.NewGuid(),
+                Questions = {
+                            new Question
+                            {
+                                Id = Guid.NewGuid(),
+                                IsRequired = false,
+                                QuestionText = "Question 1",
+                                ResponseType = ResponseType.RawText,
+                                Options = {
+                                    new Option{ Id = Guid.NewGuid() }
+                                },
+                                Order = 1,
+                            },
+                            new Question
+                            {
+                                Id = Guid.NewGuid(),
+                                IsRequired = false,
+                                QuestionText = "Question 2",
+                                ResponseType = ResponseType.SingleChoiceDropDown,
+                                Options = {
+                                    new Option{ Id = Guid.NewGuid(), Value = "A"},
+                                    new Option{ Id = Guid.NewGuid(), Value = "B"},
+                                    new Option{ Id = Guid.NewGuid(), Value = "C"},
+                                    new Option{ Id = Guid.NewGuid(), Value = "D"},
+                                    new Option{ Id = Guid.NewGuid(), Value = "E"},
+                                },
+                                Order = 2,
+                            },
+                            new Question
+                            {
+                                Id = Guid.NewGuid(),
+                                IsRequired = false,
+                                QuestionText = "Question 3",
+                                ResponseType = ResponseType.SingleChoiceRadio,
+                                Options = {
+                                    new Option{ Id = Guid.NewGuid(), Value = "Yes"},
+                                    new Option{ Id = Guid.NewGuid(), Value = "No"},
+                                    new Option{ Id = Guid.NewGuid(), Value = "Maybe"},
+                                },
+                                Order = 3,
+                            },
+                            new Question
+                            {
+                                Id = Guid.NewGuid(),
+                                IsRequired = false,
+                                QuestionText = "Question 4",
+                                ResponseType = ResponseType.MultipleChoice,
+                                Options = {
+                                    new Option{ Id = Guid.NewGuid(), Value = "Pepperoni"},
+                                    new Option{ Id = Guid.NewGuid(), Value = "Sausage"},
+                                    new Option{ Id = Guid.NewGuid(), Value = "Chicken"},
+                                    new Option{ Id = Guid.NewGuid(), Value = "Mushrooms"},
+                                },
+                                Order = 4,
+                            },
+                            new Question
+                            {
+                                Id = Guid.NewGuid(),
+                                IsRequired = false,
+                                QuestionText = "Question 5",
+                                ResponseType = ResponseType.Date,
+                                Options = {
+                                    new Option{ Id = Guid.NewGuid() },
+                                },
+                                Order = 5,
+                            },
+                    }
+            };
+
+            this.Forms.Add(form);
+            this.SaveChanges();
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
